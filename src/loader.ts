3d0d4a96e1c9e31f094f470cd99c61db57667f12
Abraham Agiri <abraham@mono.co>
2024-02-15 10:19:23 +0100

    feat: add grpc response interceptors

--- src/loader.ts
+++ src/loader.ts
@@ -7,9 +7,21 @@ import { GrpcError } from './errors'
 import { formatValidationErrors } from './util'
 import { ValidationError } from 'class-validator'
 
+type GrpcResponseType = Record<any, any>
+type GrpcMiddleware =
+  | Array<(call: ServerUnaryCall<any, any>) => Promise<any>>
+  | any
+type GrpcInterceptor = Array<
+  (
+    callRequest: ServerUnaryCall<any, any>,
+    response: GrpcResponseType
+  ) => Promise<any>
+>
+
 interface ServiceControllerOptions {
   controllers: Array<new (...args: any[]) => any>
-  middlewares: Function[]
+  middlewares?: GrpcMiddleware
+  interceptors?: GrpcInterceptor
 }
 
 export class GrpcLoader {
@@ -18,20 +30,44 @@ export class GrpcLoader {
     this.serviceName = serviceName
   }
 
-  private executeMiddleware(middlewares: Function[], call: any) {
-    if (middlewares.length) {
-      for (let index = 0; index < middlewares.length; index++) {
-        const middleware = middlewares[index]
-        middleware(call)
+  private async execute(
+    functions: any[],
+    call: ServerUnaryCall<any, any>,
+    data = {}
+  ) {
+    if (!functions) throw new GrpcError('Invalid middleware error', 500, 7)
+    if (functions.length) {
+      for (let index = 0; index < functions.length; index++) {
+        const executor = functions[index]
+        if (executor) {
+          const res = await executor(call, data)
+          Object.assign(data, res)
+        }
       }
     }
-    return
+    return data
+  }
+
+  private executeMiddleware(
+    middlewares: GrpcMiddleware,
+    call: ServerUnaryCall<any, any>
+  ) {
+    return this.execute(middlewares, call)
+  }
+
+  private executeInterceptors(
+    interceptors: GrpcInterceptor,
+    call: ServerUnaryCall<any, any>,
+    response: GrpcResponseType
+  ) {
+    return this.execute(interceptors, call, response)
   }
 
   private rpcConverter(
     controller: Record<any, any>,
     controllerPrototype: Object,
-    middlewares: Function[]
+    middlewares?: GrpcMiddleware,
+    interceptors?: GrpcInterceptor
   ): UntypedServiceImplementation {
     const convertedProcedures: any = {}
     Object.entries(controllerPrototype).forEach(([functionName]) => {
@@ -40,8 +76,12 @@ export class GrpcLoader {
         respond: sendUnaryData<any>
       ) => {
         try {
-          this.executeMiddleware(middlewares, call)
-          const data = await controller[functionName](call)
+          if (middlewares && middlewares.length)
+            await this.executeMiddleware(middlewares, call)
+          let data = await controller[functionName](call)
+          if (interceptors && interceptors.length) {
+            data = await this.executeInterceptors(interceptors, call, data)
+          }
           return respond(null, data)
         } catch (err) {
           if (err instanceof GrpcError && err?.grpcCode !== 0) {
@@ -61,13 +101,14 @@ export class GrpcLoader {
   }
 
   loadRPC(options: ServiceControllerOptions): UntypedServiceImplementation {
-    const { controllers, middlewares } = options
+    const { controllers, middlewares, interceptors } = options
     const procedure = {}
     controllers.forEach((controller) => {
       const convertedProcedures = this.rpcConverter(
         new controller(),
         controller.prototype,
-        middlewares
+        middlewares,
+        interceptors
       )
       Object.assign(procedure, convertedProcedures)
     })
